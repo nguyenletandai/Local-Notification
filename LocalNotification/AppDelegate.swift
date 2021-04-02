16554f97dcdaec9af3d5f195705c320773f86dea
Đại Nguyễn <nguyenletandai@gmail.com>
2021-04-02 09:06:06 +0700

    register

--- LocalNotification/AppDelegate.swift
+++ LocalNotification/AppDelegate.swift
@@ -14,7 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        registerForNotifications()
         return true
+    }
+    
+    func registerForNotifications() {
+        UNUserNotificationCenter.current()
+            .requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            }
     }
 
 
