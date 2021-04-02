f5482e420822a666ea2bfb8794ec7b9a45125568
Đại Nguyễn <nguyenletandai@gmail.com>
2021-04-02 08:42:32 +0700

    add project

---
+++ LocalNotification/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  LocalNotification
+//
+//  Created by Đại Nguyễn on 01/04/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
